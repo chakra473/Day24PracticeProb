5c30a1714cc8d97f047f69e4ad16371f40d5e2e4
chakravarthy <m.chakra473@gmail.com>
2022-04-10 14:37:26 +0530

    initiated

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day24PracticeProb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LambdaExpression!");
+        }
+    }
+}
